927d532cbb1e443d2ac2b00fd17e6f4ad33ebfe6
tz3528 <2386431408@qq.com>
2026-01-25 20:15:53 +0800

    fix:添加缺少的类型和属性文件引用，解决了编译不通过的问题

--- EzCompile/Midend/Dialects/Comp/lib/CompDialect.cpp
+++ EzCompile/Midend/Dialects/Comp/lib/CompDialect.cpp
@@ -11,3 +11,45 @@
 //===----------------------------------------------------------------------===//
 
 #include "CompDialect.h"
+#include "CompOps.h"
+
+#include "mlir/IR/DialectImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "mlir/IR/Builders.h"
+
+#include "CompDialect.cpp.inc"
+
+namespace ezcompile::comp {
+using ::mlir::Type;
+using ::mlir::Attribute;
+using ::mlir::Builder;
+}
+
+
+#define GET_TYPEDEF_CLASSES
+#include "CompTypes.cpp.inc"
+#undef GET_TYPEDEF_CLASSES
+
+#define GET_ATTRDEF_CLASSES
+#include "CompAttributes.cpp.inc"
+#undef GET_ATTRDEF_CLASSES
+
+void ezcompile::comp::CompDialect::initialize() {
+	addOperations<
+#define GET_OP_LIST
+#include "CompOps.cpp.inc"
+#undef GET_OP_LIST
+	>();
+
+	addTypes<
+#define GET_TYPEDEF_LIST
+#include "CompTypes.cpp.inc"
+#undef GET_TYPEDEF_LIST
+	>();
+
+	addAttributes<
+#define GET_ATTRDEF_LIST
+#include "CompAttributes.cpp.inc"
+#undef GET_ATTRDEF_LIST
+	>();
+}
--- EzCompile/Midend/Dialects/Comp/lib/CompOps.cpp
+++ EzCompile/Midend/Dialects/Comp/lib/CompOps.cpp
@@ -12,3 +12,22 @@
 
 #include "CompOps.h"
 
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/Support/LogicalResult.h"
+
+#define GET_OP_CLASSES
+#include "CompOps.cpp.inc"
+
+namespace ezcompile::comp {
+
+mlir::LogicalResult ApplyInitOp::verify() { return mlir::success(); }
+mlir::LogicalResult DimOp::verify() { return mlir::success(); }
+mlir::LogicalResult DirichletOp::verify() { return mlir::success(); }
+mlir::LogicalResult EnforceBoundaryOp::verify() { return mlir::success(); }
+mlir::LogicalResult FieldOp::verify() { return mlir::success(); }
+mlir::LogicalResult ForTimeOp::verify() { return mlir::success(); }
+mlir::LogicalResult SampleOp::verify() { return mlir::success(); }
+mlir::LogicalResult UpdateOp::verify() { return mlir::success(); }
+
+}
